0cf09d228cd7bc3a73eb77693792e14b54b865e1
StepikHybl <stepan.hyb1@email.cz>
2025-12-01 16:32:43 +0100

    ptr

--- print_ptr.c
+++ print_ptr.c
@@ -3,7 +3,8 @@
 int ft_print_ptr(void *p)
 {
     int len = 0;
-    if (!p)
+
+    if (p == NULL)
         return (write(1, "0x0", 3));
     len += write(1, "0x", 2);
     ft_putnbr_base((unsigned long)p, "0123456789abcdef", &len);
